8b5d1eb24b069d9ad27eae0df8c09f7c3a6be66f
afiqaimanafr <aaafr98@gmail.com>
2022-04-03 12:45:35 +0800

    update: latest

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -26,6 +26,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 8,
+    overflow: "hidden",
   },
   button: { flex: 1 },
   innerContainer: {
