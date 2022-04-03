423f7614e1a4eacaac8fd8dc97bf59f670210dda
afiqaimanafr <aaafr98@gmail.com>
2022-04-03 12:32:42 +0800

    update: latest

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,8 +1,12 @@
 import { FlatList } from "react-native";
-import { CATEGORIES } from "../data/dummy-data";
 
-function renderCategoryItem(item) {
-  return;
+import { CATEGORIES } from "../data/dummy-data";
+import CategoryGridTile from "../components/CategoryGridTile";
+
+function renderCategoryItem(itemData) {
+  return (
+    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+  );
 }
 
 function CategoriesScreen() {
