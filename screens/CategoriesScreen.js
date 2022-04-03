8f36c7dbdbb353a8e887440e807eb81be649ad9c
afiqaimanafr <aaafr98@gmail.com>
2022-04-03 12:30:36 +0800

    update: latest

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,7 +1,18 @@
+import { FlatList } from "react-native";
 import { CATEGORIES } from "../data/dummy-data";
 
-function CategoriesScreen() {
+function renderCategoryItem(item) {
   return;
+}
+
+function CategoriesScreen() {
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+    />
+  );
 }
 
 export default CategoriesScreen;
