77d55c0b9870f1195437d979fde02cde1d797f8b
afiqaimanafr <aaafr98@gmail.com>
2022-04-04 10:54:15 +0800

    update: latest

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,17 +1,24 @@
-import { View, Text, StyleSheet, Pressable, Image } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  Image,
+  Platform,
+} from "react-native";
 
 function MealItem({ title, imageUrl, duration, complexity, affordability }) {
   return (
-    <View>
+    <View style={styles.mealItem}>
       <Pressable>
         <View>
           <Image style={styles.image} source={{ uri: imageUrl }} />
           <Text style={styles.title}>{title}</Text>
         </View>
-        <View>
-          <Text>{duration}m</Text>
-          <Text>{complexity.toUpperCase()}</Text>
-          <Text>{affordability.toUpperCase()}</Text>
+        <View style={styles.details}>
+          <Text style={styles.detailItem}>{duration}m</Text>
+          <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+          <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
         </View>
       </Pressable>
     </View>
@@ -21,6 +28,18 @@ function MealItem({ title, imageUrl, duration, complexity, affordability }) {
 export default MealItem;
 
 const styles = StyleSheet.create({
+  mealItem: {
+    margin: 16,
+    borderRadius: 8,
+    overflow: "hidden",
+    backgroundColor: "white",
+    elevation: 4,
+    shadowColor: "black",
+    shadowOpacity: 0.35,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 16,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+  },
   image: {
     width: "100%",
     height: 200,
@@ -29,5 +48,16 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
     fontSize: 18,
+    margin: 8,
+  },
+  details: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 8,
+  },
+  detailItem: {
+    marginHorizontal: 4,
+    fontSize: 12,
   },
 });
