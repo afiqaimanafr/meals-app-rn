c16b00bbd2ae74dc2eddaae61b065c51d3516371
afiqaimanafr <aaafr98@gmail.com>
2022-04-04 12:43:28 +0800

    update: latest

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,12 +1,23 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
+
+import { MEALS } from "../data/dummy-data";
 
 function MealDetailScreen({ route }) {
   const mealId = route.params.mealId;
+
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+  
   return (
     <View>
-      <Text>This is the Meal Detail Screen {mealId}</Text>
+      <Image />
+      <Text></Text>
+      <View></View>
+      <Text>Ingredients</Text>
+      <Text>Steps</Text>
     </View>
   );
 }
 
 export default MealDetailScreen;
+
+const styles = StyleSheet.create({});
