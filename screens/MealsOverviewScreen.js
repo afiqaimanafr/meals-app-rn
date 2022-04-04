6f51848bceac86652271a06c600a43a811d610d0
afiqaimanafr <aaafr98@gmail.com>
2022-04-04 11:18:59 +0800

    update: latest

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { View, FlatList, StyleSheet } from "react-native";
 
 import { MEALS, CATEGORIES } from "../data/dummy-data";
@@ -10,11 +11,13 @@ function MealsOverviewScreen({ route, navigation }) {
     return mealItem.categoryIds.indexOf(catId) >= 0;
   });
 
-  const categoryTitle = CATEGORIES.find(
-    (category) => category.id === catId
-  ).title;
+  useEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId
+    ).title;
 
-  navigation.setOptions({ title: categoryTitle });
+    navigation.setOptions({ title: categoryTitle });
+  }, [catId, navigation]);
 
   function renderMealItem(itemData) {
     const item = itemData.item;
