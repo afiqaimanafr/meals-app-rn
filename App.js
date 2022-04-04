1aec206508e0559e5dc97cae5dfd424400e4228a
afiqaimanafr <aaafr98@gmail.com>
2022-04-04 11:11:45 +0800

    update: latest

--- App.js
+++ App.js
@@ -30,7 +30,12 @@ export default function App() {
           <Stack.Screen
             name="MealsOverview"
             component={MealsOverviewScreen}
-            options={{}}
+            options={({ route, navigation }) => {
+              const catId = route.params.categoryId;
+              return {
+                title: catId,
+              };
+            }}
           />
         </Stack.Navigator>
       </NavigationContainer>
