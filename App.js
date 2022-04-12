41a2f0cf81289cf0d32b0552bb1f17fdd8c9390f
afiqaimanafr <aaafr98@gmail.com>
2022-04-12 16:03:27 +0800

    update: latest

--- App.js
+++ App.js
@@ -14,7 +14,13 @@ const Drawer = createDrawerNavigator();
 
 function DrawerNavigator() {
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "#351401" },
+        headerTintColor: "white",
+        sceneContainerStyle: { backgroundColor: "#3f2f25" },
+      }}
+    >
       <Drawer.Screen name="Categories" component={CategoriesScreen} />
       <Drawer.Screen name="Favorites" component={FavoritesScreen} />
     </Drawer.Navigator>
@@ -37,7 +43,7 @@ export default function App() {
             name="Drawer"
             component={DrawerNavigator}
             options={{
-              title: "All Categories",
+              headerShown: false,
             }}
           />
           <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
