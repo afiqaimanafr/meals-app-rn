4c58ecdda0a1a10397cb6a33e672b08b4a0eee4a
afiqaimanafr <aaafr98@gmail.com>
2022-04-13 01:58:23 +0800

    update: latest

---
+++ store/redux/favorites.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const favoritesSlice = createSlice({
+  name: "favorites",
+  initialState: { ids: [] },
+  reducers: {
+    addFavorite: (state, action) => {
+      state.ids.push(action.payload.id);
+    },
+    removeFavorite: (state, action) => {
+      state.ids.splice(state.ids.indexOf(action.payload.id), 1);
+    },
+  },
+});
