c43108e8a2b9e599de8390ac2fafab5dea0fce40
afiqaimanafr <aaafr98@gmail.com>
2022-04-13 14:46:01 +0800

    update: latest

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,7 +1,16 @@
+import { useContext } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
+import MealsList from "../components/MealsList/MealsList";
+import { MEALS } from "../data/dummy-data";
+
 function FavoritesScreen() {
-  return <Text>The Favorites Screen</Text>;
+  const favoriteMealsCtx = useContext(FavoritesContext);
+  const favoriteMeals = MEALS.filter((meal) =>
+    favoriteMealsCtx.ids.includes(meal.id)
+  );
+
+  return <MealsList items={favoriteMeals} />;
 }
 
 export default FavoritesScreen;
