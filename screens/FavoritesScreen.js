f971e0511b6755a43ceb94b0f94ee72341ee46cc
afiqaimanafr <aaafr98@gmail.com>
2022-04-13 14:40:42 +0800

    update: latest

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,7 +1,9 @@
-import { Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 function FavoritesScreen() {
   return <Text>The Favorites Screen</Text>;
 }
 
 export default FavoritesScreen;
+
+const styles = StyleSheet.create({});
