56eef264de8d8c9a4708e0fe62632d96d2d5ae2c
afiqaimanafr <aaafr98@gmail.com>
2022-04-13 14:50:07 +0800

    update: latest

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -10,9 +10,28 @@ function FavoritesScreen() {
     favoriteMealsCtx.ids.includes(meal.id)
   );
 
+  if (favoriteMeals.length === 0) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>You have no favourite meals yet!</Text>
+      </View>
+    );
+  }
+
   return <MealsList items={favoriteMeals} />;
 }
 
 export default FavoritesScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
